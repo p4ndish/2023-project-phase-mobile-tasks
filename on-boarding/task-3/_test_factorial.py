b4d28608c62bc985c8bdcc0ab9e8dc388d46c390
ye kolo temari <65342438+p4ndish@users.noreply.github.com>
2023-09-18 20:22:35 +0300

    Revert "Optimize factorial calculation using dynamic programming"

--- on-boarding/task-3/_test_factorial.py
+++ on-boarding/task-3/_test_factorial.py
@@ -1,25 +1,19 @@
 import unittest
 from factorial import FactorialCalculator
-
 class TestFactorialCalculator(unittest.TestCase):
-    def setUp(self):
-        self.calculator = FactorialCalculator()
-
     def test_factorial_of_0(self):
-        self.assertEqual(self.calculator.factorial(0), 1)
+        self.assertEqual(FactorialCalculator.factorial(0), 1)
 
     def test_factorial_of_1(self):
-        self.assertEqual(self.calculator.factorial(1), 1)
+        self.assertEqual(FactorialCalculator.factorial(1), 1)
 
     def test_factorial_of_5(self):
-        self.assertEqual(self.calculator.factorial(5), 120)
+        self.assertEqual(FactorialCalculator.factorial(5), 120)
 
     def test_factorial_of_10(self):
-        self.assertEqual(self.calculator.factorial(10), 3628800)
-
-    def test_factorial_of_negative_number(self):
-        with self.assertRaises(ValueError):
-            self.calculator.factorial(-1)
+        self.assertEqual(FactorialCalculator.factorial(10), 3628800)
 
 if __name__ == '__main__':
     unittest.main()
+
+
